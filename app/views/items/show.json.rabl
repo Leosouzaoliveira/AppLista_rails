f18d57580a83007ff80c283262524787db1ebcb4
Leonardo <leonardo.r3@gmail.com>
2013-04-17 12:59:44 -0300

    Primeiro commit

---
+++ app/views/items/show.json.rabl
@@ -0,0 +1,2 @@
+object @item
+attributes :id, :item, :quantidade_item
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListaDeCompra::Application.config.secret_token = '32715e98861c570037db1e1398ab50b41f6fcf2cf03668bcf892ce942628eced6ef2d2e6e3a497234e278a8bf4f0d2370a3c5b4a5f1f3bb7d0bbb9706ae64927'
